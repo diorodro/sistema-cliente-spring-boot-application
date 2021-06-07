6f2285a9249a3af7060f8a683035c4be41b362b5
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-07 12:52:26 -0300

    Deletando clientes pelo Controller

--- src/main/java/io/github/diorodro/rest/ClienteController.java
+++ src/main/java/io/github/diorodro/rest/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.diorodro.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,20 @@ public class ClienteController {
 						() -> new ResponseStatusException(HttpStatus.NOT_FOUND)
 						);
 
+	}
+
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Integer id) {
+		repository
+		.findById(id)
+		.map(cliente -> {
+			repository.delete(cliente);
+			return Void.TYPE;
+		})
+		.orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+				);
 	}
 
 
